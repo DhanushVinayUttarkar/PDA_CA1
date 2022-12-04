899b4e37b65dfd5705f3cf3843a1a3d4557ef043
DhanushVinayUttarkar <dhanushuttarkar@gmail.com>
2022-12-04 01:30:18 +0000

    New repository initial commit
    Old repository expose credentials hence this new repository was created to get around that

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+import config
+
+
+# Creates a new stock market monthly dataset using the alpha-vantage api
+def create_dataset():
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+
+    querystring = {"symbol": "AAPL", "function": "TIME_SERIES_MONTHLY", "datatype": "csv"}
+
+    # X-RapidAPI-Key contains your rapid API key
+    headers = {
+        "X-RapidAPI-Key": config.apikey,
+        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    with open('Time_Series_Stock_data.csv', 'w') as out:
+        out.write(response.text)
+
+
+def preprocess_dataset():
+    df = pd.read_csv('Time_Series_Stock_data.csv')
+    print(df.head())
+
+
+if __name__ == '__main__':
+    create_dataset()
+    preprocess_dataset()
