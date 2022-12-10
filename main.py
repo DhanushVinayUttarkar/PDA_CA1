90d9a81c9285cb345bd459cf5c19785c0d958475
DhanushVinayUttarkar <dhanushuttarkar@gmail.com>
2022-12-10 02:45:11 +0000

    added partial data pre-processing, database creation and storing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import pandas as pd
 from pymongo import MongoClient
 import config as cfg
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
 
 
 # Creates a new stock market monthly dataset using the alpha-vantage api
@@ -10,7 +12,7 @@ def create_dataset():
 
     querystring = {"symbol": "AAPL", "function": "TIME_SERIES_MONTHLY", "datatype": "csv"}
 
-    # X-RapidAPI-Key contains your rapid API key
+    # X-RapidAPI-Key contains the rapid API key
     headers = {
         "X-RapidAPI-Key": cfg.apikey,
         "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
@@ -21,42 +23,53 @@ def create_dataset():
         out.write(response.text)
 
 
-def preprocess_dataset():
+def preprocess_data():
     df = pd.read_csv('Time_Series_Stock_data.csv')
-    print(df.head())
+    # print(df.info(), "\n")
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    # print(df.info())
+    print("before sorting\n", df.head())
+    df.sort_values(by=['timestamp'], inplace=True, ascending=True)
+    print("\nafter sorting\n", df.head(), "\n")
+
+    figure(figsize=(15, 8), dpi=80, linewidth=10)
+    plt.plot(df['timestamp'], df['volume'], color="r")
+    plt.title('Missing value check')
+    plt.xlabel('Years', fontsize=14)
+    plt.ylabel('volume of shares sold', fontsize=14)
+    plt.show()
+
+    return df
 
 
-def connect_to_database():
+def create_database_and_store():
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(cfg.connection_string)
 
     # database and collection code goes here
-    db = client.sample_guides
-    coll = db.comets
+    db = client.stockmarket_timeseries
+    coll = db.Apple_stock
 
     coll.drop()
-    # insert code goes here
-    docs = [
-        {"name": "Halley's Comet", "officialName": "1P/Halley", "orbitalPeriod": 75, "radius": 3.4175, "mass": 2.2e14},
-        {"name": "Wild2", "officialName": "81P/Wild", "orbitalPeriod": 6.41, "radius": 1.5534, "mass": 2.3e13},
-        {"name": "Comet Hyakutake", "officialName": "C/1996 B2", "orbitalPeriod": 17000, "radius": 0.77671,
-         "mass": 8.8e12},
-    ]
-    result = coll.insert_many(docs)
-    # display the results of your operation
-    print(result.inserted_ids, "\n")
+    df = preprocess_data()
+    # print(df.head())
+    df.reset_index(inplace=True)
+    data_dict = df.to_dict('records')
+    # print(data_dict)
+    result = coll.insert_many(data_dict)
+    # print(client.list_database_names())
 
     # find code goes here
-    cursor = coll.find({"officialName": "1P/Halley"})
+    cursor = coll.find({"index": 3})
 
     # iterate code goes here
     for doc in cursor:
         print(doc)
 
-    # Close the connection to MongoDB when you're done.
     client.close()
 
 
 if __name__ == '__main__':
-    # create_dataset()
-    preprocess_dataset()
+    create_dataset()
+    preprocess_data()
+    create_database_and_store()
